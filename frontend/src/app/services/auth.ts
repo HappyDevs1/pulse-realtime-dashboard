2ce71fa20be2d8ee2b7f41cab6dcff22adfaa6c5
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-06-22 23:39:29 +0200

    storing jwt token in session (#13)

---
+++ frontend/src/app/services/auth.ts
@@ -0,0 +1,25 @@
+// import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const baseUrl = "http://localhost:4000";
+
+// const api = axios.create(
+//   {
+//     baseURL: baseUrl,
+//     headers: {
+//       'Content-Type': 'application/json',
+
+//     }
+//   }
+// )
+
+export const loginUser = async () => {
+  try {
+    window.location.href = `${baseUrl}/auth/google`
+
+  } catch (error) {
+    console.error("Login failed")
+  }
+}
