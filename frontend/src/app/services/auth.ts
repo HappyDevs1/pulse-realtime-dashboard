4097a906f73c8918e8619e9d698191643fa848f5
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-06-23 00:17:34 +0200

    removing unused commented code

--- frontend/src/app/services/auth.ts
+++ frontend/src/app/services/auth.ts
@@ -1,19 +1,4 @@
-// import axios from 'axios';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
 const baseUrl = "http://localhost:4000";
-
-// const api = axios.create(
-//   {
-//     baseURL: baseUrl,
-//     headers: {
-//       'Content-Type': 'application/json',
-
-//     }
-//   }
-// )
 
 export const loginUser = async () => {
   try {
