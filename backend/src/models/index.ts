a0554d0d34524742848d6d8f59248bfac5dfb9b7
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-06-29 17:20:46 +0200

    Saving user, organisation and dataset into the db and storing metadata

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -1,15 +1,20 @@
 import { Sequelize, Dialect } from "sequelize";
 import dbConfig from "../config/db";
 import UserModel from "./user.model";
+import OrganisationModel from "./organisation.model";
+import DatasetModel from "./dataset.model";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
-  dialect: dbConfig.dialect as Dialect,
+  dialect: "postgres",
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false,
+      rejectUnauthorized: false
     }
   }
 });
@@ -19,6 +24,20 @@ const db: any = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.user = UserModel(sequelize, Sequelize);
+db.user = UserModel(sequelize);
+db.organisation = OrganisationModel(sequelize);
+db.dataset = DatasetModel(sequelize);
+
+// Associations
+db.organisation.hasMany(db.user, {
+  foreignKey: "organisation_id",
+  onDelete: "SET NULL",
+  onUpdate: "CASCADE"
+});
+db.user.belongsTo(db.organisation, {
+  foreignKey: "organisation_id",
+  onDelete: "SET NULL",
+  onUpdate: "CASCADE"
+});
 
 export default db;
