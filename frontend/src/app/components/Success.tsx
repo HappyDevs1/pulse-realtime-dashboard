17ddd8cceba88b8bdd89fb0dc3489598c7a6699f
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-07-07 23:36:12 +0200

    fix: moving green checkmark into its own component to improve modularity

--- frontend/src/app/components/Success.tsx
+++ frontend/src/app/components/Success.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import React from "react";
+import CheckMarkIcon from "./CheckMarkIcon";
 
 interface SuccessPageProps {
   message: string;
@@ -19,21 +20,7 @@ const SuccessPage: React.FC<SuccessPageProps> = ({ message, buttonText, redirect
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-black text-white px-4 text-center">
       {/* Green checkmark */}
-      <div className="bg-green-600 rounded-full p-4 mb-6 shadow-lg">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-12 w-12 text-white"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M16.707 5.293a1 1 0 010 1.414L8.414 15l-4.121-4.121a1 1 0 111.414-1.414L8.414 12.586l7.293-7.293a1 1 0 011.414 0z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </div>
-
+      <CheckMarkIcon />
       {/* Message */}
       <h2 className="text-2xl font-semibold mb-4 text-green-400">{message}</h2>
 
